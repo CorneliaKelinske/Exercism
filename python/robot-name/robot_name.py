f3a256ee77e6995ea9dabdd5a181917bc77cd494
CorneliaKelinske <corneliakelinske@gmail.com>
2019-06-18 20:44:06 -0600

    test working, missing piece added, but too many duplications

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,10 +1,15 @@
-import random
+from random import shuffle
+from random import randint
 
 class Robot:
     names = []
 
     def __init__(self):
         self.name = self.pick_name()
+    
+    def reset(self):
+        self.name = self.reset_name()
+        return self.name
            
     def __repr__(self):
         return self.name
@@ -18,21 +23,36 @@ class Robot:
         name = first + second + third + fourth + fifth
         if name in Robot.names:
             name = self.pick_name()
+        Robot.names.append(name)
+        return name
+    
+    def reset_name(self):
+        first = self._pick_letter()
+        second = self._pick_letter()
+        third = self._pick_number()
+        fourth = self._pick_number()
+        fifth = self._pick_number()
+        name = first + second + third + fourth + fifth
+        if name in Robot.names:
+            name = self.pick_name()
+        Robot.names.append(name)
         return name
 
     
     def _pick_letter(self):
         letters = letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
          "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-        random.shuffle(letters)
+        shuffle(letters)
         return letters[0]
     
     def _pick_number(self):
-        num = str(random.randint(0,10))
+        num = str(randint(0,10))
         return num
     
 robbie = Robot()
 print(robbie)
+print(robbie.reset())
+print (Robot.names)
 
 
 
