0a083408213da49fdce7f7f63f43ff3780279e57
CorneliaKelinske <corneliakelinske@gmail.com>
2019-08-19 06:13:14 -0600

    my first suggestion for a solution

---
+++ python/matrix/matrix.py
@@ -0,0 +1,12 @@
+class Matrix(object):
+    def __init__(self, matrix_string):
+        self.matrix_string = matrix_string
+        splitted = self.matrix_string.split("\n")
+        re_splitted = [item.split(" ") for item in splitted]
+        self.numbers = [[int(num) for num in item] for item in re_splitted]
+
+    def row(self, index):
+        return self.numbers[index-1]
+       
+    def column(self, index):
+        return [item[index-1] for item in self.numbers]
