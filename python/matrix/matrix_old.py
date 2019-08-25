69bb69397de74efeb1d45e0b5c5eecd47112043b
CorneliaKelinske <corneliakelinske@gmail.com>
2019-08-25 06:10:16 -0600

    stored old version under matrix_old; refactored version is now under matrix.py

--- python/matrix/matrix_old.py
+++ python/matrix/matrix_old.py
@@ -1,8 +1,8 @@
 class Matrix(object):
     def __init__(self, matrix_string):
         self.matrix_string = matrix_string
-        splitted = self.matrix_string.splitlines()
-        re_splitted = [item.split() for item in splitted]
+        splitted = self.matrix_string.split("\n")
+        re_splitted = [item.split(" ") for item in splitted]
         self.numbers = [[int(num) for num in item] for item in re_splitted]
 
     def row(self, index):
@@ -11,8 +11,7 @@ class Matrix(object):
     def column(self, index):
         return [item[index-1] for item in self.numbers]
 
+m = Matrix("1 2 3\n4 5 6\n7 8 9")
 
-
-
-
-
+print(m.row(2))
+print(m.column(2))
