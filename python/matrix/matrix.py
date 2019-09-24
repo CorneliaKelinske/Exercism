896798b033b447f44753a702c3b76f8b19f9d051
CorneliaKelinske <corneliakelinske@gmail.com>
2019-09-24 06:46:55 -0600

    used .copy() instead of tuple; nested list comprehension

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -2,12 +2,10 @@ class Matrix(object):
     def __init__(self, matrix_string):
         self.matrix_string = matrix_string
         splitted = self.matrix_string.splitlines()
-        re_splitted = [item.split() for item in splitted]
-        self.numbers = [[int(num) for num in item] for item in re_splitted]
+        self.numbers = [[int(num) for num in item] for item in [item.split() for item in splitted]]
 
     def row(self, index):
-        t = tuple(self.numbers[index-1])
-        return list(t)
+       return self.numbers[index-1].copy()
        
     def column(self, index):
         return [item[index-1] for item in self.numbers]
---
+++ python/protein-translation/protein_translation_beta.py
@@ -0,0 +1,3 @@
+#I need a function called proteins that takes in a strand
+#First i need the length of the strand. return in codons. either split on index or slice?
+#no idea!
