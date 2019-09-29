d59930e4e3e857e37617da1e86609c03992e3049
CorneliaKelinske <corneliakelinske@gmail.com>
2019-09-29 17:46:16 -0600

    it is working.... sometimes

--- python/protein-translation/protein_translation_beta.py
+++ python/protein-translation/protein_translation_beta.py
@@ -1,14 +1,28 @@
 #1. step: create a regex for each protein 
 import re
-m = re.compile(r'^(AUG)(\w*)') #Methionine
-ph = re.compile(r'^(UUUUUC)(\w*)') #Phenylalanine
-l = re.compile(r'^(UUAUUG)(\w*)') #Leucine
-s = re.compile(r'^(UCUUCCUCAUCG)(\w*)') #Serine
-ty = re.compile(r'^(UAUUAC)(\w*)') #Tyrosine
-c = re.compile(r'^(UGUUGC)(\w*)') #Cysteine
-t = re.compile(r'^(UGG)(\w*)') #Tryptophan
-e = re.compile(r'^(UAA)(\w*)') #STOP
+m = re.compile(r'^(AUG)(?P<rest>\w*)') #Methionine
+ph = re.compile(r'^(UUUUUC)(?P<rest>\w*)') #Phenylalanine
+l = re.compile(r'^(UUAUUG)(?P<rest>\w*)') #Leucine
+s = re.compile(r'^(UCUUCCUCAUCG)(?P<rest>\w*)') #Serine
+ty = re.compile(r'^(UAUUAC)(?P<rest>\w*)') #Tyrosine
+c = re.compile(r'^(UGUUGC)(?P<rest>\w*)') #Cysteine
+t = re.compile(r'^(UGG)(?P<rest>\w*)') #Tryptophan
+e = re.compile(r'^(UAA)(?P<rest>\w*)') #STOP
 
-match = m.search('AUG')
 
-print(match)
+code = {m : "Methionine", ph : "Phenylalanine", l : "Leucine", s : "Serine", ty : "Tyrosine", c :"Cysteine", t : "Tryptophan", e : "STOP"}
+
+def proteins(strang):
+    all_proteins = []
+    for key in code:
+        match = key.search(strang)
+        if match:
+            if key == e:
+                return all_proteins
+            else:
+                all_proteins.append(code[key])
+                strang = match.group("rest")
+                proteins(strang)
+
+print (proteins("UGGUAA"))              
+
