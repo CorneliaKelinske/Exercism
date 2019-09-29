96d7f3cac64ae1f16cc5e59b9872fa02c29642c9
CorneliaKelinske <corneliakelinske@gmail.com>
2019-09-29 17:23:40 -0600

    preparatory work

--- python/protein-translation/protein_translation_beta.py
+++ python/protein-translation/protein_translation_beta.py
@@ -1,14 +1,14 @@
 #1. step: create a regex for each protein 
 import re
-Methionine = re.compile(r'^(AUG)(\w*)')
-Phenylalanine = re.compile(r'^(UUUUUC)(\w*)')
-Leucine = re.compile(r'^(UUAUUG)(\w*)')
-Serine = re.compile(r'^(UCUUCCUCAUCG)(\w*)')
-Tyrosine = re.compile(r'^(UAUUAC)(\w*)')
-Cysteine = re.compile(r'^(UGUUGC)(\w*)')
-Tryptophan = re.compile(r'^(UGG)(\w*)')
-End_of_strang = re.compile(r'^(UAA)(\w*)')
+m = re.compile(r'^(AUG)(\w*)') #Methionine
+ph = re.compile(r'^(UUUUUC)(\w*)') #Phenylalanine
+l = re.compile(r'^(UUAUUG)(\w*)') #Leucine
+s = re.compile(r'^(UCUUCCUCAUCG)(\w*)') #Serine
+ty = re.compile(r'^(UAUUAC)(\w*)') #Tyrosine
+c = re.compile(r'^(UGUUGC)(\w*)') #Cysteine
+t = re.compile(r'^(UGG)(\w*)') #Tryptophan
+e = re.compile(r'^(UAA)(\w*)') #STOP
 
-match = Methionine.search('AUG')
+match = m.search('AUG')
 
 print(match)
