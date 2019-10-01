127dac27079f4a1a18a0be714322045f8d9c1372
CorneliaKelinske <corneliakelinske@gmail.com>
2019-10-01 06:44:42 -0600

    further working on this project. logic is not working. leaving this for future reference

--- python/protein-translation/protein_translation_beta.py
+++ python/protein-translation/protein_translation_beta.py
@@ -1,29 +1,41 @@
 #1. step: create a regex for each protein 
 import re
-m = re.compile(r'^(AUG)(?P<rest>\w*)') #Methionine
-ph = re.compile(r'^(UUUUUC)(?P<rest>\w*)') #Phenylalanine
-l = re.compile(r'^(UUAUUG)(?P<rest>\w*)') #Leucine
-s = re.compile(r'^(UCUUCCUCAUCG)(?P<rest>\w*)') #Serine
-ty = re.compile(r'^(UAUUAC)(?P<rest>\w*)') #Tyrosine
-c = re.compile(r'^(UGUUGC)(?P<rest>\w*)') #Cysteine
-t = re.compile(r'^(UGG)(?P<rest>\w*)') #Tryptophan
-e = re.compile(r'^(UAA)(?P<rest>\w*)') #STOP
+m = re.compile(r'(^AUG)(?P<rest>\w*)') #Methionine
+ph = re.compile(r'(^UUUUUC)(?P<rest>\w*)') #Phenylalanine
+l = re.compile(r'(^UUAUUG)(?P<rest>\w*)') #Leucine
+s = re.compile(r'(^UCUUCCUCAUCG)(?P<rest>\w*)') #Serine
+ty = re.compile(r'(^UAUUAC)(?P<rest>\w*)') #Tyrosine
+c = re.compile(r'(^UGUUGC)(?P<rest>\w*)') #Cysteine
+t = re.compile(r'(^UGG)(?P<rest>\w*)') #Tryptophan
+e = re.compile(r'(^UAA)(?P<rest>\w*)') #STOP
 
 
 code = {m : "Methionine", ph : "Phenylalanine", l : "Leucine", s : "Serine", ty : "Tyrosine", c :"Cysteine", t : "Tryptophan", e : "STOP"}
+all_proteins = []
 
-def proteins(strang):
-    all_proteins = []
+def proteins(strand):
+   
     for key in code:
-        match = key.search(strang)
+        match = key.search(strand)
+       
         if match:
+            print(match.group())
             if key == e:
                 break
             else:
                 all_proteins.append(code[key])
-                strang = match.group("rest")
-                proteins(strang)
+                #print(f"all proteins: {all_proteins}")
+                rest = match.group("rest")
+                if rest:
+                    strand = rest
+                    print(strand)
+                    proteins(strand)
+                else:
+                    break
     return all_proteins
 
-print (proteins("UGGUGUUAUUAAUGGUUU"))              
+print (proteins("AUGUUUUUCUAA"))
+#print (proteins("UGGAUGF"))
+                           
+   
 
