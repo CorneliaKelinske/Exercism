33f0daa127290ef841c83d553fb4dc8a09fe30ab
CorneliaKelinske <corneliakelinske@gmail.com>
2019-10-03 07:34:22 -0600

    this might be it

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -12,16 +12,19 @@ e = re.compile(r'(^UAA|^UAG|^UGA)(?P<rest>\w*)') #STOP
 
 code = {m : "Methionine", ph : "Phenylalanine", l : "Leucine", s : "Serine", ty : "Tyrosine", c :"Cysteine", t : "Tryptophan", e : "STOP"}
 all_proteins = []
+used_keys = []
 
 def proteins(strand):
     for key in code:
         match = key.search(strand)
         print(f"this is the input {strand}")
 
-        if match and key != e:
+        if match and key != e and key not in used_keys:
             all_proteins.append(code[key])
+            used_keys.append(key)
             rest =  match.group("rest")
             print(f"this is the rest {rest}")
+            
 
             strand = rest
             proteins(strand)
@@ -29,4 +32,4 @@ def proteins(strand):
       
     return all_proteins
 
-print(proteins("UUUUGG"))
+print(proteins("UGGUUU"))
