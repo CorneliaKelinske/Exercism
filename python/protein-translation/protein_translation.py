26cec984d6b8c1254f479a3e970e3b8dd8952a92
CorneliaKelinske <corneliakelinske@gmail.com>
2019-10-08 06:59:56 -0600

    final version of this exercise with regex. might re-solve it without regex at some point

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,3 +1,7 @@
+#I wanted to solve this using regex. If I run each test individually, my solution passes all of them. 
+#However, if I run more than 1 test at the same time, my code only passes the first test.
+#I am wondering why that is and if there is a better regex solution.
+
 import re
 
 m = re.compile(r'(^AUG)(?P<rest>\w*)') #Methionine
@@ -17,19 +21,16 @@ used_keys = []
 def proteins(strand):
     for key in code:
         match = key.search(strand)
-        #print(f"this is the input {strand}")
+        
 
         if match and key != e and key not in used_keys:
             all_proteins.append(code[key])
             used_keys.append(key)
             rest =  match.group("rest")
-            #print(f"this is the rest {rest}")
-            
-
             strand = rest
             proteins(strand)
                                     
       
     return all_proteins
 
-print(proteins("UGGUAGUGG"))
+print(proteins("UGGUGUUAUUAAUGGUUU"))
