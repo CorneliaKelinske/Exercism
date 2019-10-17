fea0af55037f3b27817da8824d239b61cbcf4627
CorneliaKelinske <corneliakelinske@gmail.com>
2019-10-17 06:57:10 -0600

    refactored the hamming_distance exercise

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,15 +1,13 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError ("strand_a and strand_b must have the same length")
-    hamming_distance = 0
-    y = 0
-    for char in strand_a:
-        if char != strand_b[y]:
+    hamming_distance = 0    
+    for i in range(len(strand_a)):
+        if strand_a[i] != strand_b[i]:
             hamming_distance += 1
-        y += 1
     return hamming_distance
 
-#print(distance("GGACGGATTCTG", "AGGACGGATTCT"))
+
 
 
 
