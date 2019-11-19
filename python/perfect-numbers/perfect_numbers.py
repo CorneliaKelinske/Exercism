0653eeeb863b1be33b47776aafec33c3e65c0511
CorneliaKelinske <corneliakelinske@gmail.com>
2019-11-19 11:37:14 -0700

    made some progress. not all the tests are passing yet

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,13 @@
 def classify(number):
-    pass
+    factors = [num for num in range (1, number) if number % num == 0]
+    sum_factors = sum(factors)
+    if sum_factors == number:
+        return "perfect"
+    elif sum_factors > number:
+        return "abundnat"
+    return "deficient"
+
+
+
+
+print(classify(6))
