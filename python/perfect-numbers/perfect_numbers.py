3a1a8658d556dee846854117e7e71ab975682c0d
CorneliaKelinske <corneliakelinske@gmail.com>
2019-11-19 12:05:16 -0700

    all done

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,13 +1,15 @@
 def classify(number):
     factors = [num for num in range (1, number) if number % num == 0]
     sum_factors = sum(factors)
-    if sum_factors == number:
+    if number <= 0:
+        raise ValueError ("Not a natural number")
+    elif sum_factors == number:
         return "perfect"
     elif sum_factors > number:
-        return "abundnat"
+        return "abundant"
     return "deficient"
 
 
 
 
-print(classify(6))
+#print(classify(-1))
