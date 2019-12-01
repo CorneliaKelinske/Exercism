1c2834b95b39a2ce7de92436f7c672912f128d80
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-01 08:30:25 -0700

    better

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,15 +1,5 @@
 def find_anagrams(word, candidates):
-    candidates = [item.upper() for item in candidates]       
-    print(candidates)
-
-    for item in candidates:
-        if item == word.upper():
-            candidates.remove(item)
-    if candidates[0] == word.upper():
-        candidates.pop(0)
     
-    print(candidates)
-        
-    return [item for item in candidates if sorted(item) == sorted(word.upper())]
+    return [item for item in candidates if item.upper() != word.upper() and sorted(item.upper()) == sorted(word.upper())]
 
 print(find_anagrams("BANANA", ["BANANA", "Banana", "banana"]))
