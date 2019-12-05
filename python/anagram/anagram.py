1e8ab68cd4fed88c5092e4db42a595387670ff9d
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-05 07:01:13 -0700

    getting started on the saddle_points exercise

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,5 +1,4 @@
-def find_anagrams(word, candidates):
-    
+def find_anagrams(word, candidates):    
     return [item for item in candidates if item.upper() != word.upper() and sorted(item.upper()) == sorted(word.upper())]
 
-print(find_anagrams("BANANA", ["BANANA", "Banana", "banana"]))
+
---
+++ python/saddle-points/saddle_points.py
@@ -0,0 +1,5 @@
+def saddle_points(matrix):
+    for item in matrix:
+        for num in item if num >
+
+    pass
