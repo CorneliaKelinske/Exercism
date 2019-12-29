5f49400c139b8bb5f574c635cd91be9d904149c8
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-29 11:00:12 -0800

    finished refactoring twelve days of Christmas

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -5,7 +5,7 @@ ordinals = [
     ]
 
 gift_list = [
-    "a Partridge in a Pear Tree.", "two Turtle Doves, and ", "three French Hens, ",
+    "a Partridge in a Pear Tree.", "two Turtle Doves,", "three French Hens, ",
     "four Calling Birds, ", "five Gold Rings, ", "six Geese-a-Laying, ",
     "seven Swans-a-Swimming, ", "eight Maids-a-Milking, ", "nine Ladies Dancing, ",
     "ten Lords-a-Leaping, ", "eleven Pipers Piping, ", "twelve Drummers Drumming, "
@@ -15,17 +15,21 @@ gift_list = [
 def gifts(num):
    gift = gift_list[num - 1]
    
-   if num > 1:
+   if num == 2:
+       result = gift + " and " + str(gifts(num-1))
+   elif num > 1:
         result = gift + str(gifts(num-1))
         
    else:
         result = gift
    return result
 
+def maybe_and(num):
+    if num == 2:
+        return "and"
+    return ""
+
+
 def recite(start_verse, end_verse):   
     return [(f"On the {ordinals[verse-1]} day of Christmas my true love gave to me: {gifts(verse)}") for verse in range(start_verse, end_verse+1)]
 
-#print(recite(1,10))
-print(recite(1, 1))
-print(recite(1, 2))
-#print(recite(2, 5))
