371595c22f010b257e5d4ad010ce03aaad8ee3ce
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-28 16:07:09 -0800

    cleaned up the twelve days of Christmas

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,61 +1,31 @@
-import inflect
-p = inflect.engine()
+ordinals = [
+    "first", "second", "third", "fourth",
+    "fifth", "sixth", "seventh", "eighth",
+    "ninth", "tenth", "eleventh", "twelfth"
+    ]
 
-def nth(num):
-    # if num == 1:
-    #     return "first" 
-    # elif num == 2:
-    #     return "second"
-    # elif num == 3:
-    #     return "third"
-    # elif num == 5:
-    #     return "fifth"
-    # elif num == 8:
-    #     return "eighth"
-    # elif num == 9:
-    #     return "ninth"
-    # elif num == 12:
-    #     return "twelfth"
-    # else:
-        return f"{p.ordinal(p.number_to_words(num))}"
+gift_list = [
+    "a Partridge in a Pear Tree.", "two Turtle Doves, and ", "three French Hens, ",
+    "four Calling Birds, ", "five Gold Rings, ", "six Geese-a-Laying, ",
+    "seven Swans-a-Swimming, ", "eight Maids-a-Milking, ", "nine Ladies Dancing, ",
+    "ten Lords-a-Leaping, ", "eleven Pipers Piping, ", "twelve Drummers Drumming, "
+]
+
 
 def gifts(num):
-    if num == 1:
-        gift = "a Partridge in a Pear Tree."
-    elif num == 2:
-        gift = "two Turtle Doves, and "
-    elif num == 3:
-        gift = "three French Hens, "
-    elif num == 4:
-        gift = "four Calling Birds, "
-    elif num == 5:
-        gift = "five Gold Rings, "
-    elif num == 6:
-        gift = "six Geese-a-Laying, "
-    elif num == 7:
-        gift = "seven Swans-a-Swimming, "
-    elif num == 8:
-        gift = "eight Maids-a-Milking, "
-    elif num == 9:
-        gift = "nine Ladies Dancing, "
-    elif num == 10:
-        gift = "ten Lords-a-Leaping, "
-    elif num == 11:
-        gift = "eleven Pipers Piping, "
-    else:
-        gift = "twelve Drummers Drumming, "
-    
-    if num > 1:
+   gift = gift_list[num - 1]
+   
+   if num > 1:
         result = gift + str(gifts(num-1))
         
-    else:
+   else:
         result = gift
-    return result
+   return result
 
-def recite(start_verse, end_verse):
-    return [(f"On the {nth(verse)} day of Christmas my true love gave to me: {gifts(verse)}") for verse in range(start_verse, end_verse+1)]
+def recite(start_verse, end_verse):   
+    return [(f"On the {ordinals[verse-1]} day of Christmas my true love gave to me: {gifts(verse)}") for verse in range(start_verse, end_verse+1)]
 
-print(recite(1,10))
-#print(recite(8, 8))
-#print(recite(1, 2))
+#print(recite(1,10))
+print(recite(1, 1))
+print(recite(1, 2))
 #print(recite(2, 5))
