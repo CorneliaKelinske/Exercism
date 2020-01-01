dba4fdb3d8c8b49777f096cf0dafbde71900d6c8
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-31 16:55:21 -0800

    working on the saddle_points exercise; just trying some stuff

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,9 +1,28 @@
+# def saddle_points(matrix):
+#     coordinates =[[index2,index1] for index1,value1 in enumerate(matrix) for index2,value2 in enumerate(value1)]
+#     return coordinates
+
+
+# def is_saddle_number(matrix, x, y):
+#     pass
+
+# print(saddle_points([[4, 5, 4], [3, 5, 5], [1, 5, 4, 5]]))
+
+ 
+
+# def row(matrix, index):
+#     return matrix[index].copy()
+
+# def column(matrix, index):
+#     return [item[index] for item in matrix]
+
 def saddle_points(matrix):
-    coordinates =[[index2,index1] for index1,value1 in enumerate(matrix) for index2,value2 in enumerate(value1)]
-    return coordinates
+    row_maxes = []
+    for row in matrix:
+        for index, item in enumerate(row):
+            if item == max(row):
+                print (item)
+                row_maxes.append(index)
+    return row_maxes
 
-
-def is_saddle_number(matrix, x, y):
-    pass
-
-print(saddle_points([[4, 5, 4], [3, 5, 5], [1, 5, 4, 5]]))
+print(saddle_points([[4, 5, 6], [3, 5, 5], [1, 5, 4, 5]]))
