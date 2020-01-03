22e225f616ff8c0d0faf7745d1b10a81baf5fa6a
CorneliaKelinske <corneliakelinske@gmail.com>
2020-01-03 06:31:32 -0800

    1 failure eliminated; 1 more to go

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,17 +1,3 @@
-# def saddle_points(matrix):
-#     coordinates =[[index2,index1] for index1,value1 in enumerate(matrix) for index2,value2 in enumerate(value1)]
-#     return coordinates
-
-
-# def is_saddle_number(matrix, x, y):
-#     pass
-
-# print(saddle_points([[4, 5, 4], [3, 5, 5], [1, 5, 4, 5]]))
-
- 
-
-# def row(matrix, index):
-#     return matrix[index].copy()
 
 def column(m, i):
      return [item[i] for item in m]
@@ -23,12 +9,15 @@ def saddle_points(matrix):
         if (len(item)) < len(max(matrix, key=len)):
             item.append(0)
     
-    for row in matrix:
+    for idx, row in enumerate(matrix):
+       
         for ind, item in enumerate(row):
             if item == max(row):
-                #print (item)
-                row_maxes.append((matrix.index(row),ind))
+                
+                #print(item, idx)
+                row_maxes.append((idx,ind))
     for item in row_maxes:
+        
         if matrix[item[0]][item[1]] == min(column(matrix,item[1])):
             saddle_points.append(item)
        
@@ -38,4 +27,4 @@ def saddle_points(matrix):
     
     return [{"row" : item[0]+1, "column" : item[1]+1} for item in saddle_points]
 
-print(saddle_points([[4, 5, 4], [3, 5, 5], [1, 5, 4]]))
+print(saddle_points([[2], [1], [4], [1]]))
