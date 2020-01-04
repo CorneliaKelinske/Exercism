fc73e7122db03174e33d09006df19210012e4db9
CorneliaKelinske <corneliakelinske@gmail.com>
2020-01-04 07:30:28 -0800

    finished the saddle points exercise

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -7,24 +7,17 @@ def saddle_points(matrix):
     saddle_points = []
     for item in matrix:
         if (len(item)) < len(max(matrix, key=len)):
-            item.append(0)
+            raise ValueError("irregular matrix")
     
-    for idx, row in enumerate(matrix):
-       
+    for idx, row in enumerate(matrix):       
         for ind, item in enumerate(row):
             if item == max(row):
-                
-                #print(item, idx)
                 row_maxes.append((idx,ind))
-    for item in row_maxes:
-        
+
+    for item in row_maxes:        
         if matrix[item[0]][item[1]] == min(column(matrix,item[1])):
             saddle_points.append(item)
-       
-
- 
-
-    
+   
     return [{"row" : item[0]+1, "column" : item[1]+1} for item in saddle_points]
 
-print(saddle_points([[2], [1], [4], [1]]))
+print(saddle_points([[1, 2, 3], [3, 1, 2], [2, 3, 1]]))
