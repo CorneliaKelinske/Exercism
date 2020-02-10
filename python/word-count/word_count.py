b3d6150a3b95c87c0e308f6eed78ea9a91bb891f
CorneliaKelinske <corneliakelinske@gmail.com>
2020-02-10 13:00:14 -0800

    got rid off the regex. using word.strip()instead

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,1 +1,34 @@
 import string
+
+
+def counter(list, input_word):
+    count = 0
+
+    for item in list:
+        if item == input_word:
+            count += 1
+    return count
+
+
+def count_words(sentence):
+    sentence = sentence.lower()
+    for char in string.punctuation:
+        
+        if char != "'":
+            sentence = sentence.replace(char, ' ')
+    
+    
+
+    sen_list = [word.strip("'") for word in sentence.split()]
+
+    words = []
+    results = {}
+    for word in sen_list:
+        if word not in words:
+            words.append(word)
+            results.update({word:  counter(sen_list, word)})
+
+    return results
+
+
+print(count_words("Joe can't tell between 'large' and large."))
