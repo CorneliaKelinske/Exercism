ca8b8c8603b8e77b793c7871669912bd74ecb133
CorneliaKelinske <corneliakelinske@gmail.com>
2020-02-29 07:29:44 -0800

    it works but I have no idea what output is expected

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -1,37 +1,35 @@
 
 
-# def products(num1, num2):
-#     all_products = {}
-#     for num_a in range(num1, num2+1):
-#         for num_b in range(num_a, num2+1):
-#             all_products.update({num_a * num_b: (num_a, num_b)})
-#     return all_products
 
 def is_palindrome(num):
     if str(num) == str(num)[::-1]:
         return True
 
-# def all_palindromes(min_factor, max_factor):
-#     results = []
-#     for key in products(min_factor, max_factor):
-#         if is_palindrome(key):
-#             results.append(key)
-#     return results
 
-# def largest(min_factor, max_factor):
-#     all_products = {}
-#     palindromes = {}
-
-#     for num_a in range(min_factor, max_factor+1):
-#         for num_b in range(num_a, max_factor+1):
-#             all_products.update({num_a * num_b: (num_a, num_b)})
-
-#     for key in all_products:
-#         if is_palindrome(key):
-#             palindromes.update({key : all_products[key]})
-#     return max(palindromes)
 
 def smallest (min_factor, max_factor):
+    all_products = []
+    palindromes = []
+    results = []
+
+    for num_a in range(min_factor, max_factor+1):
+        for num_b in range(num_a, max_factor+1):
+            all_products.append( [num_a * num_b, (num_a, num_b)])
+
+    for item in all_products:
+        if is_palindrome(item[0]):
+            palindromes.append(item)
+    
+    palindromes_only = [item[0] for item in palindromes]
+    
+    for item in palindromes:
+        if item[0] == min(palindromes_only):
+            results.append(item)
+
+    
+    return {"value" : results[0][0], "factors" : [item[1] for item in results]}
+
+def largest (min_factor, max_factor):
     all_products = []
     palindromes = []
     results = []
@@ -51,9 +49,8 @@ def smallest (min_factor, max_factor):
             results.append(item)
 
     
-    return results
-    
-    # smallest_palin = min(palindromes)
+    return {"value" : results[0][0], "factors" : [item[1] for item in results]}
     
 
 print(smallest(1, 9))
+print(largest(1,9))
