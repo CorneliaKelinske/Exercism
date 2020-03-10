0f6926d03165012452f073143c284ad75e41b794
CorneliaKelinske <corneliakelinske@gmail.com>
2020-03-10 10:00:47 -0700

    fine tuning time next

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,19 +1,33 @@
-def equal_sides(numbers):    
+def remove_duplicates(numbers):    
     return list(dict.fromkeys(numbers))
 
 
 def equilateral(sides):
-    pass
+    if len(remove_duplicates(sides)) == 1:
+        return True
+    return False
 
 
 def isosceles(sides):
-    pass
+    if len(remove_duplicates(sides))>= 2:
+        return True
+    return False
 
 
 def scalene(sides):
-    pass
+    if len(remove_duplicates(sides))== 3:
+        return True
+    return False
 
 
-print(equal_sides([1,2,2]))
-print(equal_sides([1,2,4]))
-print(equal_sides([2,2,2]))
+# print(equilateral([1,2,2]))
+# print(equilateral([1,2,4]))
+# print(equilateral([2,2,2]))
+
+print(isosceles([1,2,2]))
+print(isosceles([2,3,4]))
+print(isosceles([2,2,2]))
+
+print(scalene([1,2,2]))
+print(scalene([1,2,4]))
+print(scalene([2,2,2]))
