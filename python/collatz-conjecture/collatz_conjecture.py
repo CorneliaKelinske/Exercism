d4b6d4789a344ed6306c7bd842347f8c64d76a49
CorneliaKelinske <corneliakelinske@gmail.com>
2020-03-19 07:51:12 -0700

    worked on collatz. But code is killing my computer. probably not a good idea to use a while loop...

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,12 @@
 def steps(number):
-    pass
+    steps = 0
+    while number != 1:
+        if number %2 == 0:
+            number = number/2
+            steps +=1
+        else:
+            number = 3*number +1
+            steps +=1
+    return steps
+
+print(steps(12))
