a99f09879c877694385f86ffefe9b472ed3cac37
CorneliaKelinske <corneliakelinske@gmail.com>
2020-04-02 12:37:58 -0700

    trying to solve the acronym exercise using a regex

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,23 +1,30 @@
-def abbreviate(words):
-    words = words.replace("-", " ")
-    print(words)
-    words = words.split(" ")
-    words = [item for item in words if item != ""]
-    print(words)
+# def abbreviate(words):
+#     words = words.replace("-", " ")
+#     print(words)
+#     words = words.split(" ")
+#     words = [item for item in words if item != ""]
+#     print(words)
 
-    # for item in words:
-    #     if item.isalpha() == False:
-    #         words.remove(item)
-    result = []
-    for item in words:
-        if item[0].isalpha():
-            result.append(item[0])
-        else:
-            result.append(item[1])
+#     # for item in words:
+#     #     if item.isalpha() == False:
+#     #         words.remove(item)
+#     result = []
+#     for item in words:
+#         if item[0].isalpha():
+#             result.append(item[0])
+#         else:
+#             result.append(item[1])
     
-    # return words
+#     # return words
 
-    return "".join(result).upper()
+#     return "".join(result).upper()
+
+import re
+def abbreviate(words):
+    not_letter = re.compile(r'[^(a-z)|(A-Z)]')
+    words = re.sub(not_letter," ", words)
+    return words
+
 
 
 print(abbreviate("Something - _I made up from thin air"))
