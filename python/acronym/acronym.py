30072d0037d4d63ec75ed424139a77e599faa109
CorneliaKelinske <corneliakelinske@gmail.com>
2020-04-03 07:24:44 -0700

    made it look nice

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,38 +1,17 @@
-# def abbreviate(words):
-#     words = words.replace("-", " ")
-#     print(words)
-#     words = words.split(" ")
-#     words = [item for item in words if item != ""]
-#     print(words)
 
-#     # for item in words:
-#     #     if item.isalpha() == False:
-#     #         words.remove(item)
-#     result = []
-#     for item in words:
-#         if item[0].isalpha():
-#             result.append(item[0])
-#         else:
-#             result.append(item[1])
-    
-#     # return words
-
-#     return "".join(result).upper()
 
 import re
+
 def abbreviate(words):
     words = words.replace("'", "")
     not_letter = re.compile(r'[^(a-z)|(A-Z)]')
-    words = re.sub(not_letter," ", words)
+    words = re.sub(not_letter, " ", words)
     words = words.split(" ")
     words = [item for item in words if item != ""]
 
     result = []
     for item in words:
-        if item[0].isalpha():
-            result.append(item[0])
+        result.append(item[0])
     return "".join(result).upper()
 
 
-
-print(abbreviate("Something - _I made up from thin air"))
