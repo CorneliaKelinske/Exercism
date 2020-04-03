e27f32f020e85e006709c54194cea5290d950590
CorneliaKelinske <corneliakelinske@gmail.com>
2020-04-03 07:22:45 -0700

    last problem solved

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -21,6 +21,7 @@
 
 import re
 def abbreviate(words):
+    words = words.replace("'", "")
     not_letter = re.compile(r'[^(a-z)|(A-Z)]')
     words = re.sub(not_letter," ", words)
     words = words.split(" ")
