abc3bcb292a12e6db2fc5f5a8356c8dfdbf44cd4
CorneliaKelinske <corneliakelinske@gmail.com>
2020-04-14 12:39:58 -0700

    getting started on the prime-factors exercise

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,15 @@
 def square(number):
-    pass
+    if number not in range(1,65):
+        raise ValueError("number needs to be between 1 and 64")
+    elif number == 1:
+        return 1
+    return 2**(number-1)
 
 
 def total():
-    pass
+    total = 0
+    number = 1
+    while number <= 64:
+        total += square(number)
+        number+=1
+    return total
