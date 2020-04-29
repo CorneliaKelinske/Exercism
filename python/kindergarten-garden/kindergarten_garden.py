5837f128e046319dfbc46aeda6ed5029cc30d8a0
CorneliaKelinske <corneliakelinske@gmail.com>
2020-04-29 07:21:07 -0700

    initialized my Garden class

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,16 @@
+plant_names: {"R": "Radishes", "C": "Clover", "G": "Grass", "V": "Violets"}
+
+
 class Garden:
-    def __init__(self, diagram, students):
+    def __init__(self, diagram, students=[
+            "Alice", "Bob", "Charlie", "David",
+            "Eve", "Fred", "Ginny", "Harriet",
+            "Ileana", "Joseph", "Kincaid", "Larry"
+    ]):
+        self.diagram = diagram
+        self.students = students
         pass
+
+
+garden = Garden("RC\nGG")
+print(garden.diagram)
