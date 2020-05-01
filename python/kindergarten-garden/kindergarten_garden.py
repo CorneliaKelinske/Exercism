fc2196e3b2d3b20308a9e6fd2889546a40c7959b
CorneliaKelinske <corneliakelinske@gmail.com>
2020-04-30 17:54:00 -0700

    added another dict

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,4 +1,7 @@
 plant_names: {"R": "Radishes", "C": "Clover", "G": "Grass", "V": "Violets"}
+kids_indexe: {"Alice":(0,1), "Bob" : (2,3) , "Charlie" : (4,5), "David" : (6,7),
+            "Eve" : (8,9), "Fred" : (10, 11), "Ginny" : (12, 13), "Harriet" : (14, 15),
+            "Ileana" : (16, 17), "Joseph" : (18, 19), "Kincaid" : (20, 21), "Larry" : (22, 23)}
 
 
 class Garden:
@@ -9,7 +12,7 @@ class Garden:
     ]):
         self.diagram = diagram
         self.students = students
-        pass
+        
 
 
 garden = Garden("RC\nGG")
