df3813a87779b598e7a55afac5c8d91c242309fc
CorneliaKelinske <corneliakelinske@gmail.com>
2020-05-17 18:10:18 -0700

    playing around wiht the prime_factors problem

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,9 +1,26 @@
 
-def is_prime(number):
-    primes = []
-    for num in range (2, number+1):
+#def is_prime(number):
+#    primes = []
+#    for num in range (2, number+1):
 
+
+#def factors(value):
 
 
 def factors(value):
-    pass
+    prime_factors = []
+    num = 2
+    while num <= value:
+        print(num)
+        if value % num == 0:
+            while value % num ==0:
+                prime_factors.append(num)
+                print(prime_factors)
+                value = value/num
+                print(value)
+            num+=1
+            print(num)
+        num+=1
+    return prime_factors
+    
+print(factors(60))
