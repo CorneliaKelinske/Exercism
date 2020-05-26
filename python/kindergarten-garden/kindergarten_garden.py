b1d496f317ed9f9cc9790fbc01de246bb107d1d5
CorneliaKelinske <corneliakelinske@gmail.com>
2020-05-26 07:11:59 -0700

    refactor continues

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -19,22 +19,24 @@ class Garden:
             index += 2
         return indexes
 
-    def plants(self, name):
+    def plants(self, students):
             rows = self.diagram.splitlines()
             kids_indexes = self.assign_index()
+            plants_short = {}
 
-            plants_short = [(item[kids_indexes[name][0]], item[kids_indexes[name][1]]) for item in rows]
-            plants_full = []
+            for name in students:
+                plants_short ={name:(item[kids_indexes[name][0]], item[kids_indexes[name][1]]) for item in rows}
+            # plants_full = []
 
-            for item in plants_short:
-                for letter in item:
-                    plants_full.append(PLANT_NAMES[letter])
+            # for item in plants_short:
+            #     for letter in item:
+            #         plants_full.append(PLANT_NAMES[letter])
         
-            return plants_full
+            return plants_short
 
  #before: used one name, got the plants for that name, converted the plants.
  #next step: do the same thing for each kid in students. append kid + the plants that go with it to a dict
  #update dict with long names
      
 garden = Garden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV")
-print(garden.plants("Alice"))
+print(garden.plants("students"))
