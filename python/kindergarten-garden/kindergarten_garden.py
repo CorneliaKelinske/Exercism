3ba4844ae0014f0e4b805fc746a5d12037eb2196
CorneliaKelinske <corneliakelinske@gmail.com>
2020-05-26 09:59:54 -0700

    fixed my plants_short dict

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -8,7 +8,7 @@ class Garden:
     def __init__(self, diagram, students=STUDENTS):
         self.diagram = diagram
         self.students = students
-        self.student_plants = self.plants()
+        self.student_plants = self.students_plants()
        
 
     def assign_index(self):
@@ -20,24 +20,19 @@ class Garden:
             index += 2
         return indexes
 
-    def plants(self):
+    def students_plants(self):
             rows = self.diagram.splitlines()
             kids_indexes = self.assign_index()
             plants_short = {}
 
             for student in self.students:
-                plants_short.update({student:(item[kids_indexes[student][0]], item[kids_indexes[student][1]]) for item in rows})
-            # plants_full = []
-
-            # for item in plants_short:
-            #     for letter in item:
-            #         plants_full.append(PLANT_NAMES[letter])
-        
+                plants_short.update({student:[(item[kids_indexes[student][0]], item[kids_indexes[student][1]]) for item in rows]})       
             return plants_short
 
- #before: used one name, got the plants for that name, converted the plants.
- #next step: do the same thing for each kid in students. append kid + the plants that go with it to a dict
- #update dict with long names
+    #def plants(self, name):
+
+
+
      
 garden = Garden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV")
-print(garden.plants())
+print(garden.students_plants())
