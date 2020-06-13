3798fa3c93543fcfc1cf079eb8a7e1f11f9b6fff
CorneliaKelinske <corneliakelinske@gmail.com>
2020-06-13 07:28:10 -0700

    using the logic from the V2 solution in my V1 solution. problem is still the index issue when the diagram does not have sufficient plants.

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -3,3 +3,26 @@ STUDENTS = [
             "Alice", "Bob", "Charlie", "David",
             "Eve", "Fred", "Ginny", "Harriet",
             "Ileana", "Joseph", "Kincaid", "Larry"
+    ]
+
+class Garden:
+     def __init__(self, diagram, students=STUDENTS):
+         self.students = sorted(students)
+         self.diagram = diagram.splitlines()
+         self.garden = self.__set_up_garden__()
+    
+
+     def __set_up_garden__(self):
+        plant_lookup = {}
+        for student in self.students:
+            index = self.students.index(student)        
+            plants_short = []       
+            for item in self.diagram:            
+                plants_short.append(item[index*2])
+                plants_short.append(item[index*2 + 1])
+            plant_lookup.update({student: plants_short})
+        return plant_lookup
+
+garden = Garden("VRCGVVRVCGGCCGVRGCVCGC\nVRCCCGCRRGVCGCRVVCVGCG")
+print(garden.__set_up_garden__())
+        
