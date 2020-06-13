2d85d45b027eb8f38b79def056a7682bfb6f4bf1
CorneliaKelinske <corneliakelinske@gmail.com>
2020-06-13 07:11:03 -0700

    and back to the drawing board. need to find a way to initialize my whole garden in __init__

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -3,19 +3,3 @@ STUDENTS = [
             "Alice", "Bob", "Charlie", "David",
             "Eve", "Fred", "Ginny", "Harriet",
             "Ileana", "Joseph", "Kincaid", "Larry"
-    ]
-
-class Garden:
-     def __init__(self, diagram, students=STUDENTS):
-         self.students = sorted(students)
-         self.diagram = diagram.splitlines()
-
-     def plants(self, name):
-        index = self.students.index(name)        
-        plants_short = []       
-        for item in self.diagram:            
-            plants_short.append(item[index*2])
-            plants_short.append(item[index*2 + 1])
-            
-        return [PLANT_NAMES[x] for x in plants_short]
-
---
+++ python/kindergarten-garden/kindergarten_garden_V2.py
@@ -0,0 +1,21 @@
+PLANT_NAMES = {"R": "Radishes", "C": "Clover", "G": "Grass", "V": "Violets", "0":"Did not plant anything"}
+STUDENTS = [
+            "Alice", "Bob", "Charlie", "David",
+            "Eve", "Fred", "Ginny", "Harriet",
+            "Ileana", "Joseph", "Kincaid", "Larry"
+    ]
+
+class Garden:
+     def __init__(self, diagram, students=STUDENTS):
+         self.students = sorted(students)
+         self.diagram = diagram.splitlines()
+
+     def plants(self, name):
+        index = self.students.index(name)        
+        plants_short = []       
+        for item in self.diagram:            
+            plants_short.append(item[index*2])
+            plants_short.append(item[index*2 + 1])
+            
+        return [PLANT_NAMES[x] for x in plants_short]
+
