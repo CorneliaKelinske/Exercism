23cf00d6760f744a44b3dbf9a9a2a41f346b79ac
CorneliaKelinske <corneliakelinske@gmail.com>
2020-07-01 17:10:44 -0700

    starting on my add_student function

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,12 +1,21 @@
 class School:
     def __init__(self):
-        pass
+        self.all_students = {}
+        
 
     def add_student(self, name, grade):
-        pass
+        if grade in self.all_students:
+            self.all_students[grade].append(name)
+        else:
+            self.all_students.update({grade:[name]})
+        return self.all_students
 
     def roster(self):
         pass
 
     def grade(self, grade_number):
         pass
+
+school = School()
+print(school.add_student(name="Aimee", grade=2))
+print(school.add_student(name="Bert", grade=2))
