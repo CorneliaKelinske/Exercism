971549888ac3c87d6d199d087b130984f4d94240
CorneliaKelinske <corneliakelinske@gmail.com>
2020-07-03 14:48:00 -0700

    trying to figure out my roster

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -4,21 +4,25 @@ class School:
         
 
     def add_student(self, name, grade):
+    
         if grade in self.all_students:
             self.all_students[grade].append(name)
         else:
             self.all_students.update({grade:[name]})
-        return self.all_students
+        students_new = self.all_students
+        return students_new
 
     def roster(self):
-        pass
+        roster = sorted(self.all_students)
+        return roster
 
-    def grade(self, grade_number):
-        return sorted(self.all_students[grade_number])
+    #def grade(self, grade_number):
+        #return self.all_students
          
         
 
 school = School()
 print(school.add_student(name="Hubert", grade=2))
 print(school.add_student(name="Bert", grade=2))
-print(school.grade(2))
+print(school.add_student(name="Elsa", grade =1))
+print(school.roster())
