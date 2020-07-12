b0a8cda44fb516ac8f67730a8bba054d3604c961
CorneliaKelinske <corneliakelinske@gmail.com>
2020-07-12 07:19:38 -0700

    added got going on this one

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,13 @@
 def response(hey_bob):
-    pass
+    if hey_bob.isupper():
+        if hey_bob[-1] == "?":
+            return "Calm down, I know what I'm doing!"
+        return "Whoa, chill out!"
+    elif hey_bob[-1] == "?":
+        return "Sure"
+    return "Whatever"
+
+print(response("HELLO?"))
+print(response("HELLO"))
+
+print(response("How?"))
