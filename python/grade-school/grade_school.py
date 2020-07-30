f3b6aa0cedd1b706120ac8bfe2f3a8f23b0c74c6
CorneliaKelinske <corneliakelinske@gmail.com>
2020-07-30 06:53:28 -0700

    learned about the get() function

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,23 +1,21 @@
 from collections import defaultdict
-from collections import OrderedDict
-
 
 class School:
     def __init__(self):
         self.all_students = defaultdict(list)
-
-    def add_student(self, name, grade):
+        
+    def add_student(self, name, grade):    
         self.all_students[grade].append(name)       
         return self.all_students
-    
+
     def roster(self):
         roster_complete = []
-        roster = OrderedDict(sorted(self.all_students.items()))
-        for k, v in roster.items():
-            roster_complete.extend(sorted(v))       
+        roster = [sorted(self.all_students[number]) for number in sorted(self.all_students)]
+        for item in roster:
+            for name in item:
+                roster_complete.append(name)
         return roster_complete
 
-    def grade(self, grade_number):       
-        return sorted(self.all_students[grade_number])       
-        
-
+    def grade(self, grade_number):
+        return sorted(self.all_students.get(grade_number, []))
+   
