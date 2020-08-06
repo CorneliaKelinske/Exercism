4082fb2c3f4dad470002374320dced4acb44587e
CorneliaKelinske <corneliakelinske@gmail.com>
2020-08-06 14:27:30 -0700

    making some progress

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,18 @@
 class Luhn:
     def __init__(self, card_num):
-        pass
+        self.num = [item for item in card_num if item.isnumeric()]
+        
 
     def valid(self):
-        pass
+        if len(self.num) <=1:
+            return False
+        else:
+            self.num.reverse()
+            #doubled = [int(item)*2 for item in self.num if self.num.index(item) %2 !=0]
+            doubled = [int(item)*2 if not index %2 ==0 else item for index, item in enumerate(self.num)]
+            return doubled
+            
+
+
+card = Luhn("0545")
+print(card.valid())
