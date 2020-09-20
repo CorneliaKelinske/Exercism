b0faea2a2f16a0383961116ebe326c7cc981cbdf
CorneliaKelinske <corneliakelinske@gmail.com>
2020-09-20 09:26:27 -0700

    logic is working so far

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -2,10 +2,18 @@ class Clock:
     def __init__(self, hour, minute):
         if minute < 60:
             self.minute = minute
-        self.minute 
+        else:
+            self.minute = minute%60
+            hour = int(minute/60) + hour
+        if hour < 23:
+            self.hour = hour
+        else:
+            self.hour = hour%24
+
 
     def __repr__(self):
-        pass
+        return(f"{self.hour}:{self.minute}")
+
 
     def __eq__(self, other):
         pass
@@ -15,3 +23,6 @@ class Clock:
 
     def __sub__(self, minutes):
         pass
+
+test_clock = Clock(100,0)
+print(test_clock)
