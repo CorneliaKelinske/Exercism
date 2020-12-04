53b8d3e4b1a7a6de518706c5f91c209ad1227777
CorneliaKelinske <corneliakelinske@gmail.com>
2020-12-04 06:57:00 -0800

    worked on my plan to incorporate a separate convert method. so far, things seem to be working.

--- python/clock/clock_new_new.py
+++ python/clock/clock_new_new.py
@@ -1,32 +1,41 @@
-def __init__(self, hour, minute):
-    self.total_minutes = hour * 60 + minute
+class Clock:
+    def __init__(self, hour, minute):
+        self.total_minutes = hour * 60 + minute
 
 
-def __convert__(self, minutes):
-    minute = minutes%60
+    def __convert__(self, minutes):
+        minute = minutes % 60
         #print(minute)
-    if minutes >= 0:
-        total_hours = int(minutes/60)
-    else:
-        if minutes%60 == 0:                
-            total_hours=int(minutes/60)
+        if minutes >= 0:
+            total_hours = int(minutes/60)
+        else:
+            if minutes % 60 == 0:
+                total_hours = int(minutes/60)
             else:
-                total_hours = int(self.total_minutes/60)-1
-        print(total_hours)
+                total_hours = int(minutes/60)-1
+        #print(total_hours)
 
         if total_hours >= 0:
             #print("going through the hour is greater than or 0 loop")
             if total_hours <= 23:
                 #print("going through the hour is smaller than 23 loop")
-                self.hour = total_hours
+                hour = total_hours
             else:
                 #print("going through the hour is greater than 23 loop")
-                self.hour = total_hours%24
+                self.hour = total_hours % 24
         else:
             #print("going through the hour is negative loop")
             if abs(total_hours) <= 24:
                 #print("going through the absolute value of the hour is smaller or equal to 24 loop")
-                self.hour = 24 - abs(total_hours)
+                hour = 24 - abs(total_hours)
             else:
                 #print("going through the absolute value of the hour is greater than 24 loop")
-                self.hour = 24 - abs(total_hours)%24
+                hour = 24 - abs(total_hours) % 24
+
+        return(f"{str(hour).zfill(2)}:{str(minute).zfill(2)}")
+
+    def __repr__(self):
+        return self.__convert__(self.total_minutes)
+
+test_clock = Clock(0, 45)
+print(test_clock)
