68f2dbea4e6b735c3601bd0dd52e84ab353beac4
CorneliaKelinske <corneliakelinske@gmail.com>
2020-12-24 06:54:52 -0800

    starting on the palindrome products again

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -1,56 +1,6 @@
+def largest(min_factor, max_factor):
+    pass
 
 
-
-def is_palindrome(num):
-    if str(num) == str(num)[::-1]:
-        return True
-
-
-
-def smallest (min_factor, max_factor):
-    all_products = []
-    palindromes = []
-    results = []
-
-    for num_a in range(min_factor, max_factor+1):
-        for num_b in range(num_a, max_factor+1):
-            all_products.append( [num_a * num_b, (num_a, num_b)])
-
-    for item in all_products:
-        if is_palindrome(item[0]):
-            palindromes.append(item)
-    
-    palindromes_only = [item[0] for item in palindromes]
-    
-    for item in palindromes:
-        if item[0] == min(palindromes_only):
-            results.append(item)
-
-    
-    return (results[0][0], [item[1] for item in results])
-
-def largest (min_factor, max_factor):
-    all_products = []
-    palindromes = []
-    results = []
-
-    for num_a in range(min_factor, max_factor+1):
-        for num_b in range(num_a, max_factor+1):
-            all_products.append( [num_a * num_b, (num_a, num_b)])
-
-    for item in all_products:
-        if is_palindrome(item[0]):
-            palindromes.append(item)
-    
-    palindromes_only = [item[0] for item in palindromes]
-    
-    for item in palindromes:
-        if item[0] == max(palindromes_only):
-            results.append(item)
-
-    
-    return (results[0][0],  [item[1] for item in results])
-    
-
-print(smallest(1, 9))
-print(largest(1,9))
+def smallest(min_factor, max_factor):
+    pass
--- python/palindrome-products/palindrome_products_test.py
+++ python/palindrome-products/palindrome_products_test.py
@@ -41,33 +41,33 @@ class PalindromeProductsTest(unittest.TestCase):
         self.assertEqual(value, 1002001)
         self.assertFactorsEqual(factors, [[1001, 1001]])
 
-    # def test_find_the_largest_palindrome_from_four_digit_factors(self):
-    #     value, factors = largest(min_factor=1000, max_factor=9999)
-    #     self.assertEqual(value, 99000099)
-    #     self.assertFactorsEqual(factors, [[9901, 9999]])
+    def test_find_the_largest_palindrome_from_four_digit_factors(self):
+        value, factors = largest(min_factor=1000, max_factor=9999)
+        self.assertEqual(value, 99000099)
+        self.assertFactorsEqual(factors, [[9901, 9999]])
 
-    # def test_empty_result_for_smallest_if_no_palindrome_in_the_range(self):
-    #     value, factors = smallest(min_factor=1002, max_factor=1003)
-    #     self.assertIsNone(value)
-    #     self.assertFactorsEqual(factors, [])
+    def test_empty_result_for_smallest_if_no_palindrome_in_the_range(self):
+        value, factors = smallest(min_factor=1002, max_factor=1003)
+        self.assertIsNone(value)
+        self.assertFactorsEqual(factors, [])
 
-    # def test_empty_result_for_largest_if_no_palindrome_in_the_range(self):
-    #     value, factors = largest(min_factor=15, max_factor=15)
-    #     self.assertIsNone(value)
-    #     self.assertFactorsEqual(factors, [])
+    def test_empty_result_for_largest_if_no_palindrome_in_the_range(self):
+        value, factors = largest(min_factor=15, max_factor=15)
+        self.assertIsNone(value)
+        self.assertFactorsEqual(factors, [])
 
-    # def test_error_result_for_smallest_if_min_is_more_than_max(self):
-    #     with self.assertRaisesWithMessage(ValueError):
-    #         value, factors = smallest(min_factor=10000, max_factor=1)
+    def test_error_result_for_smallest_if_min_is_more_than_max(self):
+        with self.assertRaisesWithMessage(ValueError):
+            value, factors = smallest(min_factor=10000, max_factor=1)
 
-    # def test_error_result_for_largest_if_min_is_more_than_max(self):
-    #     with self.assertRaisesWithMessage(ValueError):
-    #         value, factors = largest(min_factor=2, max_factor=1)
+    def test_error_result_for_largest_if_min_is_more_than_max(self):
+        with self.assertRaisesWithMessage(ValueError):
+            value, factors = largest(min_factor=2, max_factor=1)
 
     def assertFactorsEqual(self, actual, expected):
         self.assertEqual(set(map(frozenset, actual)), set(map(frozenset, expected)))
 
-    #Utility functions
+    # Utility functions
     def assertRaisesWithMessage(self, exception):
         return self.assertRaisesRegex(exception, r".+")
 
