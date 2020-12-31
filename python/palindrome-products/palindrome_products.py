6fe5e3b4c177eb28fd7b4305fd55a8ec733b3023
CorneliaKelinske <corneliakelinske@gmail.com>
2020-12-31 06:57:22 -0800

    brainstormed some more

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -3,11 +3,14 @@ def largest(min_factor, max_factor):
     for num1 in range(min_factor, max_factor+1):
         for num2 in range(min_factor, max_factor+1):
             if num1*num2 in products:                
-                products[num1*num2].append((num1, num2))
+                products[num1*num2].append([num1, num2])
             else:
-                products.update({num1*num2 : [(num1, num2)]})
+                products.update({num1*num2 : [[num1, num2]]})
     palindromes = [key for key in products if str(key) == str(key)[-1]]
-    return palindromes
+    for item in products[max(palindromes)]:
+        item.sort()
+    
+    return products[max(palindromes)]
 
 print(largest(1,9))   
 
--- python/palindrome-products/play_around.py
+++ python/palindrome-products/play_around.py
@@ -1,12 +1,17 @@
-numbers = range(1,4)
-products = {}
+# numbers = range(1,4)
+# products = {}
 
-for num1 in numbers:
-    for num2 in numbers:
-        products.update({num1*num2 : [(num1, num2)]})
+# for num1 in numbers:
+#     for num2 in numbers:
+#         products.update({num1*num2 : [(num1, num2)]})
 
 
-products = {1: [(1, 1)], 2: [(2, 1)], 3: [(3, 1)], 4: [(2, 2)], 6: [(3, 2)], 9: [(3, 3)]}
-products[1][len(products[1])-1] = "blue"
+# products = {1: [(1, 1)], 2: [(2, 1)], 3: [(3, 1)], 4: [(2, 2)], 6: [(3, 2)], 9: [(3, 3)]}
+# products[1][len(products[1])-1] = "blue"
 
 #print(products)
+
+# test_list = [1, 2, 3, 4, 4]
+# new_list = list(set(test_list))
+# print(new_list)
+
