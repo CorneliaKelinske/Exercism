bfdf8ea9b5583d9d6992b992beb1c8806371e72a
CorneliaKelinske <corneliakelinske@gmail.com>
2021-01-08 10:05:30 -0800

    made my clock much much easier, after mentor told me how to

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,25 +1,7 @@
 class Clock:
-    def __init__(self, hour, minute):        
-        total_minutes = hour * 60 + minute    
-        self.minute = total_minutes % 60 
-
-        if total_minutes >= 0 or total_minutes % 60 == 0:
-            total_hours = int(total_minutes/60)       
-        else:
-            total_hours = int(total_minutes/60)-1
-        
-        if total_hours >= 0:            
-            if total_hours <= 23:                
-                self.hour = total_hours            
-            else:               
-                self.hour = total_hours % 24
-        else:            
-            if abs(total_hours) <= 24:                
-                self.hour = 24 - abs(total_hours)
-            else:                
-                self.hour = 24 - abs(total_hours) % 24
-        if self.hour == 24:
-            self.hour = 0
+    def __init__(self, hour, minute):
+        self.hour = (hour + minute // 60) % 24
+        self.minute = minute % 60
        
 
     def __repr__(self):
